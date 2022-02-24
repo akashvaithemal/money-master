cd490b52a5812f78a8f17a85dc40b9c69904fd12
akashvaithemal <sahmmed974@gmail.com>
2022-02-25 04:17:45 +0600

    js added sucessfully using function

---
+++ js/calcu.js
@@ -0,0 +1,45 @@
+function getInputValue(inputId) {
+    const incomeInput = document.getElementById(inputId);
+    const incomeAmmountText = incomeInput.value;
+    const incomeAmmount = parseFloat(incomeAmmountText); 
+    // incomeInput.value = '';
+    return incomeAmmount;
+    
+}
+
+document.getElementById('total-calc').addEventListener('click',function(){
+    const incomeAmmount = getInputValue('income-input');
+    const foodAmmount = getInputValue('food-cost');
+    const rentAmmount = getInputValue('rent-cost');
+    const clothAmmount = getInputValue('cloth-cost');
+    
+    
+    console.log(incomeAmmount)
+    console.log(foodAmmount)
+    console.log(rentAmmount)
+    console.log(clothAmmount)
+    
+    const balanceTotal = document.getElementById('total-balance');
+         balanceTotalTk= incomeAmmount-(foodAmmount+rentAmmount+clothAmmount);
+        balanceTotal.innerText = 'Total Balance: '+balanceTotalTk;
+        
+    const expenceTotal = document.getElementById('total-expence');
+        expenceTotalTk= foodAmmount+rentAmmount+clothAmmount;
+           expenceTotal.innerText='Expence: '+expenceTotalTk;
+        })
+document.getElementById('save-button').addEventListener('click',function(){
+    const saveAmmount = getInputValue('save-input');
+    console.log(saveAmmount)
+     const savingsTotal = document.getElementById('saving-ammount');
+    //  getInputValue('saving-ammount');
+     console.log(savingsTotal)
+    
+           savingsTotalTk= balanceTotalTk/saveAmmount;
+           savingsTotal.innerText= 'Savings: '+ savingsTotalTk;
+           
+        const remainingBalanceTotalTk =document.getElementById('remaining-balance');
+        remainingBalanceTotal= balanceTotalTk-savingsTotalTk;
+        remainingBalanceTotalTk.innerText= 'Remaining Balance: '+ remainingBalanceTotal;
+})
+
+
